0d1192d39838e9355c60bb56363d0fb2a1e58aed
phil468 <jdelacruzcalderon@gmail.com>
2024-06-05 17:40:50 -0500

    se cambia asset por url en video login ...login-10s.mp4

--- resources/views/vendor/adminlte/auth/auth-page.blade.php
+++ resources/views/vendor/adminlte/auth/auth-page.blade.php
@@ -18,7 +18,7 @@
 @section('body')
 
     <div class="video-background" style="background-image: url('https://carontestudio.com/img/contacto.jpg');">
-        <video class="video-background-content" src="{{asset('img/evaluacion/login-10s.mp4')}}" autoplay="true" muted="true" loop="true"></video>
+        <video class="video-background-content" src="{{url('img/evaluacion/login-10s.mp4')}}" autoplay="true" muted="true" loop="true"></video>
     </div>
 
     <div class="body-bg d-flex h-100 w-100 justify-content-center ">
