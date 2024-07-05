2b0e481126da20ec3079449a45f68e911b7b6235
phil468 <jdelacruzcalderon@gmail.com>
2024-07-05 09:40:44 -0500

    Añade botones de editar y eliminar con confirmación en table-actions-4.blade.php
    - Implementa botón de edición con modal para registros, utilizando Livewire para la acción de edición.
    - Integra botón de eliminación con diálogo de confirmación JavaScript para prevenir borrados accidentales, también manejado por Livewire.
    - Usa iconos FontAwesome para una representación visual intuitiva de las acciones de editar y eliminar.
    - Asegura la accesibilidad y usabilidad mejorando la experiencia de usuario en la gestión de registros.

--- resources/views/table-actions-4.blade.php
+++ resources/views/table-actions-4.blade.php
@@ -1,5 +1,7 @@
 
 <div class="btn-group">
-    <a title="Editar" data-toggle="modal" class="btn btn-vanguard" wire:click="edit({{$id}})"><i class="fa fa-edit"></i></a>
-    <a title="Eliminar" class="btn btn-danger" onclick="confirm('¿Confirma eliminar {{$id}} - {{ $name ?? '' }}? \n ¡Los datos eliminados no pueden ser recuperados!')||event.stopImmediatePropagation()" wire:click="destroy({{$id}})"><i class="fa fa-trash"></i></a>
+    <a title="Editar" data-toggle="modal" class="btn btn-lg btn-outline-vanguard" wire:click="edit({{$id}})"><i class="fa fa-edit"></i></a>
+    
+    <a title="Eliminar" class="btn btn-lg btn-outline-danger" onclick="confirm('¿Confirma eliminar {{$id}} - {{ $name ?? '' }}? \n ¡Los datos eliminados no pueden ser recuperados!')||event.stopImmediatePropagation()" wire:click="destroy({{$id}})"><i class="fa fa-trash"></i></a>
+    
 </div>
