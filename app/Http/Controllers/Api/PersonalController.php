c97de6f5bb2486fcf44f68a1b127788ca522a3e5
phil468 <jdelacruzcalderon@gmail.com>
2025-07-16 17:34:24 -0500

    feat: Agregar espacio en blanco para mejorar la legibilidad del código en PersonalController

--- app/Http/Controllers/Api/PersonalController.php
+++ app/Http/Controllers/Api/PersonalController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class PersonalController extends Controller
 {
+    
     /**
      * Obtiene los datos de todos los registros de personal para la tabla
      *
