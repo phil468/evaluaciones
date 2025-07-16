1c1ac1bd4a16b23317ab9e247f28ce43520a0d09
phil468 <jdelacruzcalderon@gmail.com>
2025-07-16 10:23:48 -0500

    feat: Agregar migración para cambiar el tipo de dato de campania_id en la tabla campanias

---
+++ database/migrations/2025_07_16_100735_alter_campania_id_campania_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCampaniaIdCampaniaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // cambiar el tipo de dato de campania_id en la tabla campanias
+        Schema::table('campanias', function (Blueprint $table) {
+            $table->unsignedBigInteger('campania_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('campanias', function (Blueprint $table) {
+            // revertir el tipo de dato a unsignedInteger
+            $table->unsignedInteger('campania_id')->change();
+        });
+    }
+}
